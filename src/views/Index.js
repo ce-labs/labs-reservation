d70a79f92e09a078128bc3df9121899dd812f55a
angelortizv <angelo2898@gmail.com>
2021-10-26 18:42:27 -0600

    [ADD] Index Navbar and Footer

--- src/views/Index.js
+++ src/views/Index.js
@@ -1,9 +1,16 @@
+/*eslint-disable*/
 import React from "react";
+import { Link } from "react-router-dom";
+
+import IndexNavbar from "../components/Navbars/IndexNavbar";
+import Footer from "../components/Footers/Footer";
 
 export default function Index() {
   return (
     <>
-        <h1>Landing Page</h1>
+      <IndexNavbar fixed />
+      <h1>asdsadsada<br/><br/><br/><br/><br/><br/><br/><br/></h1>
+      <Footer />
     </>
   );
 }
