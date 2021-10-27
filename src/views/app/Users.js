2c06f38c38910db180ab66a5ae70c71e1e64292e
angelortizv <angelo2898@gmail.com>
2021-10-27 13:26:47 -0600

    [ADD] Default Users Table Template

--- src/views/app/Users.js
+++ src/views/app/Users.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+//components 
+
+import CardTable from "../../components/Cards/CardTableUsers";
+
 export default function Users() {
   return (
-    <>
-        <h1>Users Page</h1>
-    </>
+      <div className="flex flex-wrap mt-4">
+        <div className="w-full mb-12 px-4" style={{paddingTop:'150px'}}>
+          <CardTable/>
+        </div>
+      </div>
   );
 }
