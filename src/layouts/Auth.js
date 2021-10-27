3bd5250779bba09f4ef51c989911225b6d5923e0
angelortizv <angelo2898@gmail.com>
2021-10-26 19:17:53 -0600

    [ADD] Auth Navbar and Footer

--- src/layouts/Auth.js
+++ src/layouts/Auth.js
@@ -3,8 +3,8 @@ import { Switch, Route, Redirect } from "react-router-dom";
 
 // components
 
-//import Navbar from "components/Navbars/AuthNavbar.js";
-//import FooterSmall from "components/Footers/FooterSmall.js";
+import Navbar from "../components/Navbars/AuthNavbar";
+import FooterSmall from "../components/Footers/FooterSmall";
 
 // views
 
@@ -15,14 +15,14 @@ import Login from "../views/auth/Login";
 export default function Auth() {
   return (
     <>
-      {/*<Navbar transparent />*/}
+      <Navbar transparent />
       <main>
         <section className="relative w-full h-full py-40 min-h-screen">
           <Switch>
             <Route path="/auth/login" exact component={Login} />
             <Redirect from="/auth" to="/auth/login" />
           </Switch>
-          {/*<FooterSmall absolute />*/}
+          <FooterSmall absolute />
         </section>
       </main>
     </>
