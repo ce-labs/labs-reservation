30751c41867a068ba7f525a4fdb38a543ce3f9eb
angelortizv <angelo2898@gmail.com>
2021-10-26 18:54:17 -0600

    [ADD] App description on home page

--- src/views/Index.js
+++ src/views/Index.js
@@ -9,7 +9,44 @@ export default function Index() {
   return (
     <>
       <IndexNavbar fixed />
-      <h1>asdsadsada<br/><br/><br/><br/><br/><br/><br/><br/></h1>
+      <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
+        <div className="container mx-auto items-center flex flex-wrap">
+          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
+            <div className="pt-32 sm:pt-0">
+              <h2 className="font-semibold text-4xl text-blueGray-600">
+                Sistema de Reservación de Laboratorios - Área Académica de Ingeniería en Computadores.
+              </h2>
+              <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
+                El Sistema de Reservación de Laboratorios es una herramienta que tiene como propósito principal la consulta y reserva sobre disponibilidad de los 
+                laboratorios del Área Académica de Ingeniería en Computadores. Está destinada para que operadores, profesores y personal 
+                administrativo realicen la gestión de reservaciones desde su computadora.
+              </p>
+              <div className="mt-12">
+                <a
+                  href="/auth"
+                  className="github-star ml-1 text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-darkBlue-001 active:bg-blueGray-600 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
+                >
+                  <i class="fas fa-sign-in-alt"></i> Iniciar Sesión
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <img
+          className="absolute top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860px"
+          src={require("../assets/img/pattern_react.png").default}
+          alt="..."
+        />
+      </section>  
+
+      <section className="pb-16 bg-white relative pt-32">
+        <div
+          className="-mt-20 top-0 bottom-auto left-0 right-0 w-full absolute h-20"
+          style={{ transform: "translateZ(0)" }}
+        ></div>
+      </section>
+
       <Footer />
     </>
   );
