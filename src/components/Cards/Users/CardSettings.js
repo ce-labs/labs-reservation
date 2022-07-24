46ea3719279a02249ed155a6f6fed13ba41a3eaa
Alejandro Vásquez Oviedo <alvaov@estudiantec.cr>
2022-07-24 00:29:52 -0600

    [UPDATE] Hidden password on update profile page

--- src/components/Cards/Users/CardSettings.js
+++ src/components/Cards/Users/CardSettings.js
@@ -222,21 +222,6 @@ export default function CardSettings({currentUser}) {
                   />
                 </div>
               </div>
-              <div className="w-full lg:w-6/12 px-4">
-                <div className="relative w-full mb-3">
-                  <label
-                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                  >
-                    Contraseña 
-                  </label>
-                  <input
-                    type="text"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    defaultValue={userData.password}
-                    onChange={handleInputChangeForPassword}
-                  />
-                </div>
-              </div>
               <div className="w-full lg:w-8/12 px-4" style={{marginLeft: 'auto', paddingTop:'25px'}}>
                 <a
                   className="github-star ml-1 text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-darkBlue-001 active:bg-lightBlue-600 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
